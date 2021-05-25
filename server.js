886209687f39de40c9878a6249d5023d3aac1065
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-05-25 16:50:31 +0530

    did the basic stuff

--- server.js
+++ server.js
@@ -4,10 +4,25 @@
 // Dependencies
 
 const http = require('http');
+const url = require('url')
 
 // Ther server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-  res.end('Hello world\n')
+
+  // Get the url and parse it 
+  const parsedUrl = url.parse(req.url, true)
+  // console.log(parsedUrl)
+
+  // Get the path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // Send the response
+  res.end("hello world\n")
+
+  // Log the request path
+  console.log("Req received on this path : ", trimmedPath)
+
 })
 
 
